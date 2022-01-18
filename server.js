02b0d32ff54d130ff2acd7f2e09ba67e8bf73c39
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-18 11:41:39 -0500

    query code

--- server.js
+++ server.js
@@ -27,11 +27,40 @@ const db = mysql.createConnection(
 
 
 
-
+// -----------------------------------------------
 // return all the date in the candidates table
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1 (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+// -----------------------------------------------
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
