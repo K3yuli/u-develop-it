bab2a955394a4cc05595189f5869bb3fe662e342
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-19 22:46:28 -0500

    voter table, modularize code

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,14 @@
+// import express
+const express = require('express');
+
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+
+router.use(require('./partyRoutes'));
+
+
+
+
+
+module.exports = router;
